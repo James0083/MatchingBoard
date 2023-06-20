62877f7c071af16c1a191c520c259ab22605bb82
You JaeWon <jaewon000830@gmail.com>
2023-06-20 18:26:46 +0900

    클래스이름 첫글자 대문자 수정

--- src/main/java/com/multi/matchingBoard/mainController.java
+++ src/main/java/com/multi/matchingBoard/mainController.java
@@ -5,15 +5,17 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.multi.model.RoomVO;
 
 import lombok.extern.log4j.Log4j;
 
 @Controller
-//@RequestMapping("/room")
+@RequestMapping("/room")
 @Log4j
-public class mainController {
+public class MainController {
 
 	@GetMapping(value="/createRoom")
 	public String createRoom() {
@@ -29,6 +31,21 @@ public class mainController {
 		
 		
 		return "matchingRoom/roomDetail";
+	}
+	
+	@GetMapping(value="/roomView")
+	public String roomDetail(Model m, @RequestParam(defaultValue="0") int roomId) {
+		//방번호로 해당 방 내용 가져오기
+//		RoomVO vo=this.roomService.selectBoardByIdx(roomId);
+		
+		//해당 방의 인원 내용 가져오기
+//		List<UserVO> memberArr=this.RoomService.selectMemberAll(roomId);
+		
+//		m.addAttribute("room", vo);
+//		m.addAttribute("memberArr",memberArr);
+		
+		
+		return "matchingRoom/roomView";
 	}
 	
 }

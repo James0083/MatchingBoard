fae0417f95dca45c3668f2dc3b2bdb69f4fa3d01
You JaeWon <jaewon000830@gmail.com>
2023-07-06 20:00:17 +0900

    게임목록 및 검색(장르, 제목, 인원, 플레이시간, 연령) 페이지 생성

--- src/main/java/com/multi/service/GameServiceImpl.java
+++ src/main/java/com/multi/service/GameServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.multi.mapper.GameMapper;
+import com.multi.model.GameVO;
 
 @Service
 public class GameServiceImpl implements GameService {
@@ -21,6 +22,42 @@ public class GameServiceImpl implements GameService {
 	public List<String> getAllGameNames() {
 		// TODO Auto-generated method stub
 		return gameMapper.getAllGameNames();
+	}
+
+	@Override
+	public List<GameVO> getAllGame() {
+
+		return gameMapper.getAllGame();
+	}
+
+	@Override
+	public List<GameVO> getGameListByGenre(String genre) {
+		
+		return gameMapper.getGameListByGenre(genre);
+	}
+
+	@Override
+	public List<GameVO> getFindGameTitle(String keyword) {
+		
+		return gameMapper.getFindGameTitle(keyword);
+	}
+
+	@Override
+	public List<GameVO> getFindGamePlaytime(int keyword) {
+
+		return gameMapper.getFindGamePlaytime(keyword);
+	}
+
+	@Override
+	public List<GameVO> getFindGameNum(int keyword) {
+
+		return gameMapper.getFindGameNum(keyword);
+	}
+
+	@Override
+	public List<GameVO> getFindGameAge(int keyword) {
+
+		return gameMapper.getFindGameAge(keyword);
 	}
 
 }
